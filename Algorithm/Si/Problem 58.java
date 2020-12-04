131ca63d0c0526c3c785a95a77e188d65ae9bd45
lsisi <lsisi@student.chalmers.se>
2020-12-04 16:45:29 +0800

    problem 58:)

---
+++ Algorithm/Si/Problem 58.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') 
+            i--; // 搜索首个空格
+            res.append(s.substring(i + 1, j + 1) + " "); // 添加单词
+            while(i >= 0 && s.charAt(i) == ' ') 
+            i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
+}
+//从内存的角度解析字符串的题目
+//双指针适用于哪些场景？
+//while是怎么实现循环的？
